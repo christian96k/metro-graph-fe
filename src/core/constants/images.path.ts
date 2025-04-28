2de57b8b7619b2fea8bd0a36aa110f0b54685e36
Christian Kevin Castro <christian.castro@deas.it>
2025-04-28 22:58:16 +0200

    add: metro b stations completed

--- src/core/constants/images.path.ts
+++ src/core/constants/images.path.ts
@@ -31,6 +31,36 @@ import COLLI_ALBANI from '../../assets/images/metro-a-photo/75555-Colli_Albani.j
 import FLAMINIO from '../../assets/images/metro-a-photo/81151-Flaminio.jpg';
 import SPAGNA from '../../assets/images/metro-a-photo/82366-spagna.jpg';
 
+
+import JONIO from '../../assets/images/metro-b1-photo/73755-Jonio.jpeg';
+import CONCA_DORO from '../../assets/images/metro-b1-photo/71576-Conca_d_oro.jpg';
+import LIBIA from '../../assets/images/metro-b1-photo/72685-Libia.jpg';
+import S_AGNESE_ANNIBALIANO from '../../assets/images/metro-b1-photo/72650-Sant_Agnese_Annibaliano.jpg';
+
+
+import REBIBBIA from '../../assets/images/metro-b-photo/20204-Rebibbia.jpg';
+import PIETRALATA from '../../assets/images/metro-b-photo/72482-Pietralata.jpg';
+import PONTE_MAMMOLO from '../../assets/images/metro-b-photo/73960-Ponte_Mammolo.jpg';
+import MONTI_TIBURTINI from '../../assets/images/metro-b-photo/73922-Monti_Tiburtini.jpg';
+import QUINTILIANI from '../../assets/images/metro-b-photo/82386-Quintiliani.jpg';
+import TIBURTINA from '../../assets/images/metro-b-photo/30816-Tiburtina.jpg';
+import SANTA_MARIA_DEL_SOCCORSO from '../../assets/images/metro-b-photo/71085-Santa_Maria_del_Soccorso.jpg';
+import BOLOGNA from '../../assets/images/metro-b-photo/70126-Bologna.jpg';
+import POLICLINICO from '../../assets/images/metro-b-photo/72042-Policlinico.jpg';
+import CASTRO_PRETORIO from '../../assets/images/metro-b-photo/70567-Castro_Pretorio.jpg';
+import CAVOUR from '../../assets/images/metro-b-photo/72055-Cavour.jpg';
+import COLOSSEO from '../../assets/images/metro-b-photo/70340-Colosseo.jpg';
+import CIRCO_MASSIMO from '../../assets/images/metro-b-photo/70747-Circo_Massimo.jpg';
+import PIRAMIDE from '../../assets/images/metro-b-photo/70787-Piramide.jpg';
+import GARBATELLA from '../../assets/images/metro-b-photo/71899-Garbatella.jpg';
+import BASILICA_SAN_PAOLO from '../../assets/images/metro-b-photo/71759-Basilica_San_Paolo.jpg';
+import MARCONI from '../../assets/images/metro-b-photo/78755-Marconi.jpg';
+import EUR_MAGLIANA from '../../assets/images/metro-b-photo/73163-Magliana.jpg';
+import EUR_PALASPORT from '../../assets/images/metro-b-photo/73161-Palasport.jpg';
+import EUR_FERMI from '../../assets/images/metro-b-photo/76419-Eur_fermi.jpg';
+import LAURENTINA from '../../assets/images/metro-b-photo/05022-Laurentina.jpg';
+
+
 import MTICKET from '../../assets/images/utils/metro-ticket.svg';
 
 export const IMAGES_PATH = {
@@ -67,5 +97,37 @@ export const IMAGES_PATH = {
     "75490": FURIO_CAMILLO,
     "75555": COLLI_ALBANI,
     "81151": FLAMINIO,
-    "82366": SPAGNA
+    "82366": SPAGNA,
+
+    // Metro B1 photos
+    "73755": JONIO,
+    "71576": CONCA_DORO,
+    "72685": LIBIA,
+    "72650": S_AGNESE_ANNIBALIANO,
+    
+    
+    // Metro B photos
+    "72482":PIETRALATA,
+    "20204":REBIBBIA,
+    "73960":PONTE_MAMMOLO,
+    "73922":MONTI_TIBURTINI,
+    "82386":QUINTILIANI,
+    "30816":TIBURTINA,
+    "71085":SANTA_MARIA_DEL_SOCCORSO,
+    "70126": BOLOGNA,
+    "72042": POLICLINICO,
+    "70567": CASTRO_PRETORIO,
+    "72055": CAVOUR,
+    "70340": COLOSSEO,
+    "70747": CIRCO_MASSIMO,
+    "70787": PIRAMIDE,
+    "71899": GARBATELLA,
+    "71759": BASILICA_SAN_PAOLO,
+    "78755": MARCONI,
+    "73163": EUR_MAGLIANA,
+    "73161": EUR_PALASPORT,
+    "76419": EUR_FERMI,
+    "05022": LAURENTINA,
+
+
 };
